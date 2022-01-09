2228f98edb1eaf0ce7d1e65ae599fca42cd98253
hazartaylan <57994196+hazartaylan@users.noreply.github.com>
2022-01-09 16:36:09 +0300

    Create parksensoru.ino

---
+++ parksensoru.ino
@@ -0,0 +1,30 @@
+
+#include "NewPing.h"
+#define TRIGGER_PIN 9
+#define ECHO_PIN 10
+#define MAX_DISTANCE 400  
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+float duration, distance;
+void setup() 
+{
+  Serial.begin(9600);
+}
+void loop() 
+{
+  // Send ping, get distance in cm
+  distance = sonar.ping_cm();
+  
+  // Send results to Serial Monitor
+  Serial.print("Mesafe = ");
+  
+  if (distance >= 400 || distance <= 2) 
+  {
+    Serial.println("Out of range");
+  }
+  else 
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
